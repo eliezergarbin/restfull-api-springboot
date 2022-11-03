4b4d36119e77e409e02c908fe597bfe7e5d04b05
Eliezer Nascimento <eliezergarbin1@gmail.com>
2022-11-03 19:24:43 -0300

    criando entidade User e conectando ao MongoDb

---
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.example.demo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.User;
+import com.example.demo.services.UserService;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	@Autowired
+	private UserService service;
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
