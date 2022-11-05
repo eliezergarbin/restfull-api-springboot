54b299b39efc0b3ce34ae4f3f35d67cc54151209
Eliezer Nascimento <eliezergarbin1@gmail.com>
2022-11-05 00:09:32 -0300

    criando consulta simples com query

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -1,11 +1,18 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
